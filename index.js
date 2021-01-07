1d7c24f065eae686b3479f4c91761934461c6d6d
hitsuji <hirofumi.nakamura.private@gmail.com>
2021-01-08 00:08:21 +0900

    update: top画面の表示内容を更新した

---
+++ index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        num_of_players: obj.num_of_players,
+        avg_popn_class: obj.avg_popn_class,
+        timestump: obj.timestump
+    }
+});
---
+++ src/obj.js
@@ -0,0 +1,7 @@
+const obj = 
+{
+	"num_of_players" : 281,
+	"avg_popn_class" : 94.98627686832741,
+	"timestump" : "2021/01/08 00:05:47"
+}
+; 
