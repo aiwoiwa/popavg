ff10ce59626d57b09144be9064b15166f1215bbd
hitsuji <hirofumi.nakamura.private@gmail.com>
2022-06-19 22:40:53 +0900

    update: データを更新

--- src/obj.js
+++ src/obj.js
@@ -1,7 +1,7 @@
 const obj = 
 {
-	"num_of_players" : 565,
-	"avg_popn_class" : 92.88294017699116,
-	"timestump" : "2022/06/19 20:21:50"
+	"num_of_players" : 566,
+	"avg_popn_class" : 92.8953851590106,
+	"timestump" : "2022/06/19 22:35:03"
 }
 ; 
